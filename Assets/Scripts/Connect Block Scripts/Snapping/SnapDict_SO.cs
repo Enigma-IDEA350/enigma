1f3a2ce3ae1f97e9c78da2929544ec9f6fc9218d
tsteternlieb <73751684+tsteternlieb@users.noreply.github.com>
2021-05-10 15:46:24 -0400

    Refactor + playbutton changes + other stuff

---
+++ Assets/Scripts/Connect Block Scripts/Snapping/SnapDict_SO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapDict_SO : ScriptableObject
+{
+    /// <summary>
+    /// Dictionary which has abstract block types as keys
+    /// values is a dict which maps  
+    /// </summary>
+    static readonly Dictionary<string, Dictionary<string, string[]>> lockingDictionary;
+
+}
---
+++ Assets/Scripts/Game Logic Scripts/RunMachine.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RunMachine : MonoBehaviour
+{
+    public bool BlockErrors = false;
+    private ForEachBlock[] forEachBlocks { get; set; }
+    ForEachBlock ForEachBlock;
+    ErrorHandler errorHandler;
+
+    private TextMeshProUGUI outputField;
+
+    private GameLogic gameLogic;
+
+
+    private void Start()
+    {
+        gameLogic = GetComponent<GameLogic>();
+    }
+
+    public void TestMachine()
+    {
+        outputField = gameLogic.OutputField;
+        errorHandler = gameLogic.errorHandler;
+
+        BlockErrors = false;
+        forEachBlocks = GameObject.FindObjectsOfType<ForEachBlock>();
+
+        if (forEachBlocks.Length > 1)
+        {
+            RaiseError("There are too many FOR blocks");
+            gameLogic.MessageData.Decode_Attempt = "";
+        }
+        else if (forEachBlocks.Length == 0)
+        {
+            RaiseError("You need a FOR block");
+            gameLogic.MessageData.Decode_Attempt = "";
+        }
+
+        else
+        {
+            ForEachBlock = forEachBlocks[0];
+            if (ForEachBlock.ReadblockExists)
+            {
+                string potentialMessage = ForEachBlock.Map(gameLogic.MessageData.Code);
+                if (!BlockErrors)
+                {
+                    gameLogic.editOutputText = true;
+                    gameLogic.MessageData.Decode_Attempt = potentialMessage;
+                }
+                else
+                {
+                    gameLogic.MessageData.Decode_Attempt = "";
+                }
+            }
+            else
+            {
+                errorHandler.raiseError("You are missing a WRITE block");
+                gameLogic.MessageData.Decode_Attempt = "";
+            }
+            outputField.text = gameLogic.MessageData.Decode_Attempt;
+            if (gameLogic.MessageData.Decode_Attempt.ToLower() == gameLogic.MessageData.CorrectText.ToLower())
+            {
+                gameLogic.CorrectDecode = true;
+            }
+        }
+    }
+
+
+    public void RaiseError(string error)
+    {
+        BlockErrors = true;
+        errorHandler.raiseError(error);
+    }
+}
